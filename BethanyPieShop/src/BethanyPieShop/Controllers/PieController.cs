0fd773e4e77b07e90f594450ecb264c9ecee93f6
Gunda <SG046005@cerner.net>
2017-02-25 15:04:29 -0600

    Created OverView Page

---
+++ BethanyPieShop/src/BethanyPieShop/Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanyPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(ICategoryRepository categoryRepository,IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.Pies;
+            piesListViewModel.CurrentCategory = "Cheese Cake";
+            return View(piesListViewModel);
+        }
+    }
+}
--- BethanyPieShop/src/BethanyPieShop/Startup.cs
+++ BethanyPieShop/src/BethanyPieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BethanyPieShop.Models;
 
 namespace BethanyPieShop
 {
@@ -16,6 +17,8 @@ namespace BethanyPieShop
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
         }
 
